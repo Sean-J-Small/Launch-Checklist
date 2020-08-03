6a3aa9bb27f636622d63b2113cfeb902db061644
Sean Small <seanj.small@live.com>
2020-08-03 18:30:08 -0500

    Third time's the charm

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ window.addEventListener("load", function() {
       let copilotNameInput = document.querySelector("input[name=copilotName]");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
       let cargoMassInput = document.querySelector("input[name=cargoMass]");
+      let pilotName = pilotNameInput.value;
+      let copilotName = copilotNameInput.value;
       let isFormReady = true;
       
 
@@ -26,12 +28,26 @@ window.addEventListener("load", function() {
       if (isFormReady) {
          let isShuttleReady = true;
 
+         if (!isNaN(pilotNameInput.value) || !isNaN(copilotNameInput.value)) {
+            alert("Names can't be numbers!")
+         faulty.style.visibility = "invisible";
+         // fuelStatus.innerHTML = "Not enough fuel for the journey!";
+         // launchStatus.innerHTML = "Shuttle not ready for launch";
+         // launchStatus.style.color = "Red";
+         isShuttleReady = false;
+         event.preventDefault();
+         return
+           };
       
          if (fuelLevelInput.value < 10000) {
             alert("Enter a correct fuel input!")
          faulty.style.visibility = "visible";
-         fuelStatus.innerHTML = "Not enough fuel for the journey!";
-         launchStatus.innerHTML = "Shuttle not ready for launch";
+         faulty.innerHTML = `<ol>
+         <li id="pilotStatus">${pilotName} Ready</li>
+         <li id="copilotStatus">${copilotName} Ready</li>
+         <li id="fuelStatus">Fuel level too low for launch! Fuel Up!</li>
+         <li id="cargoStatus">Cargo mass low enough for launch</li>
+         </ol>`;
          launchStatus.style.color = "Red";
          isShuttleReady = false;
          event.preventDefault();
@@ -40,8 +56,12 @@ window.addEventListener("load", function() {
         }
         if (cargoMassInput.value > 10000) {
          faulty.style.visibility = "visible";
-         cargoStatus.innerHTML = "Too much chonk on da space ship for launch! Get running!";
-         launchStatus.innerHTML = "Shuttle not ready for launch";
+         faulty.innerHTML = `<ol>
+         <li id="pilotStatus">${pilotName} Ready</li>
+         <li id="copilotStatus">${copilotName} Ready</li>
+         <li id="fuelStatus">Fuel level high enough for launch</li>
+         <li id="cargoStatus">Cargo mass too high for launch! Treadmill time!</li>
+         </ol>`;
          launchStatus.style.color = "Red";
          isShuttleReady = false;
          event.preventDefault();
@@ -49,15 +69,7 @@ window.addEventListener("load", function() {
            cargoMassInput.innerHTML = "Weight is good!";
         }
       
-        if (!isNaN(pilotNameInput.value) || !isNaN(copilotNameInput.value)) {
-         alert("Names can't be numbers!")
-      faulty.style.visibility = "invisible";
-      // fuelStatus.innerHTML = "Not enough fuel for the journey!";
-      // launchStatus.innerHTML = "Shuttle not ready for launch";
-      // launchStatus.style.color = "Red";
-      isShuttleReady = false;
-      event.preventDefault();
-        };
+        
    //   if (Number(copilotNameInput.value) === (copilotNameInput.value)) {
    //    alert("coNames can't be numbers either!")
    // faulty.style.visibility = "invisible";
